ed911205d2f80df916bbd090211a002068776240
Nicostudent <70242834+Nicostudent@users.noreply.github.com>
2022-08-16 17:07:38 -0300

    bio fixedd

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -5,9 +5,12 @@ const About = () => {
     return (
         <div className='about'>
             <p>
-            Hello, I'm Nico, I'm 34 years old. I am currently studying Javascript programming. I started a year ago in a bootcamp, being my first contact with the IT world, I found it strange and complicated. Since then it has been quite a while and I learned a lot, I would love to have my first work experience in this field, with a good group to be able to learn, grow and develop as a programmer.
-            I found that I love reading code and seeing what other people can develop, the ingenuity and logic they use.
-            I would be delighted to be hired and give my best.
+            Hi, I'm Nico, I'm 35 years old. I recently finished a Javascript programming bootcamp, if you want you can see my certificate here.
+             I started a year ago, and being my first contact with the IT world, I found it strange and complicated.
+              Since then it has been quite a while and I learned a lot, I would love to have my first work experience in this field,
+               with a good group to be able to learn, grow and develop as a programmer.
+                I found that I love reading code and seeing what other people can develop, the ingenuity and logic they use.
+                I am currently reinforcing my knowledge of javascript and learn a little react native. I would be delighted to be hired and give my best.
             </p>
             <br />
             <p>
